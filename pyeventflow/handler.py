1dab12faaee8c5572216764df0ee6075276a874b
EnderASz <olek5188@gmail.com>
2023-12-15 22:37:06 +0100

    Remove Event base class and refactor event handling

--- pyeventflow/handler.py
+++ pyeventflow/handler.py
@@ -1,75 +1,151 @@
 import abc
+import asyncio
+from collections import defaultdict
+import inspect
 import itertools
 import typing as t
-import asyncio
 
 from pyeventflow import exc
 from pyeventflow.event import (
+    EventHandlerEvent,
     LoopCloseRequestEvent,
     SubLoopsCloseRequestEvent,
-    EventLoopFlowEvent,
-    HandlersRunnerFlowEvent,
 )
+from pyeventflow.proto import EventHandlingFunction
 
-_EventT = t.TypeVar("_EventT", bound="Event")
+
+class _HandlerBase:
+    # TODO: Provide generic type for supported event types
+    _events_registry: dict[type, set[EventHandlingFunction]] = defaultdict(set)
+
+    def __init_subclass__(cls, **kwargs) -> None:
+        super().__init_subclass__(**kwargs)
+        for _, method in inspect.getmembers(cls, inspect.isfunction):
+            # TODO: Provide generic type for supported event types
+            method: EventHandlingFunction
+            if hasattr(method, "handle_events"):
+                for event_type in method.handle_events:
+                    cls._events_registry[event_type].add(method)
+
+    # TODO: Provide generic type for supported event types
+    def _get_event_handling_methods(
+        self, event: t.Any
+    ) -> t.Iterator[EventHandlingFunction]:
+        for event_type, handlers in self._events_registry.items():
+            if isinstance(event, event_type):
+                yield from iter(handlers)
+
+    # TODO: Provide generic type for supported event types
+    @staticmethod
+    def handling_method(
+        event_type: type,
+    ) -> t.Callable[[EventHandlingFunction], EventHandlingFunction]:
+        """Creates a decorator which marks given callable as handler for
+        `event_type`.
+
+        Under the hood this decorator creates a `handle_events` set of event
+        types and/or adds given `event_type` to this set.
+
+        Args:
+            event_type: type: event type
+
+        Returns:
+            Decorator marking given callable as handler for `event_type`
+        """
+
+        # TODO: Provide generic type for supported event types
+        def marker(handler: EventHandlingFunction) -> EventHandlingFunction:
+            if not hasattr(handler, "handle_events"):
+                handler.handle_events = {event_type}
+                return handler
+            handler.handle_events.add(event_type)
+            return handler
+
+        return marker
 
 
-class EventHandler(t.Generic[_EventT]):
+handling_method = _HandlerBase.handling_method
+
+
+class EventHandler(_HandlerBase):
     def __init__(self) -> None:
         self._loop_lock = asyncio.Lock()
+        # TODO: Provide generic type for supported event types
         self._event_queue: asyncio.Queue[
-            _EventT | EventLoopFlowEvent
+            EventHandlerEvent | t.Any
         ] = asyncio.Queue()
 
     @property
     def event_loop_running(self) -> bool:
         return self._loop_lock.locked()
 
-    def receive(self, event: _EventT | EventLoopFlowEvent) -> None:
+    # TODO: Provide generic type for supported event types
+    def receive(self, event: EventHandlerEvent | t.Any) -> None:
         self._event_queue.put_nowait(event)
 
     async def before_event_loop(self) -> None:
         pass
 
-    async def wait_for_event(self) -> _EventT | EventLoopFlowEvent:
+    # TODO: Provide generic type for supported event types
+    async def wait_for_event(self) -> EventHandlerEvent | t.Any:
         return await self._event_queue.get()
 
     async def _event_loop(self) -> t.AsyncGenerator[None, None]:
+        abort_reasons: list[Exception] = []
+
+        exit_loop = False
         while True:
             event = await self.wait_for_event()
-            try:
-                if (
-                    handler_method := event.get_handler_method(self)
-                ) is not None:
-                    await handler_method(event)
-            except exc.EventLoopExit:
-                break
-            except Exception as e:
+
+            methods = self._get_event_handling_methods(event)
+            results = await asyncio.gather(
+                *(method(self, event) for method in methods), return_exceptions=True
+            )
+            for result in results:
+                # INFO: Handling method can not only raise `EventLoopExit` or
+                # any other exception to exit/abort event loop, but they can
+                # also return them
+                if isinstance(result, exc.EventLoopExit):
+                    exit_loop = True
+                    continue
+                if isinstance(result, Exception):
+                    abort_reasons.append(result)
+                # Result values other than exceptions are just ignored
+
+            if len(abort_reasons) > 0:
                 raise exc.EventLoopAborted(
                     f"Event loop of {self} has been aborted, due to "
-                    f"exception occurrence."
-                ) from e
+                    f"exceptions occurrence."
+                ) from ExceptionGroup("", abort_reasons)
+            if exit_loop:
+                return
             yield
 
     async def after_event(self) -> None:
+        """Method called after the event has been processed.
+
+        This function can be used to clean up any resources that were created
+        in the `before_event` method or during event handling.
+
+        Returns:
+            None
+        """
         pass
 
     async def after_event_loop(
         self, exception: BaseException | None = None
     ) -> bool:
-        """
-        Called every time when event loop ends with EventLoopExit or any other
-        exception. If any exception other than ExitEventLoop has been raised,
-        it is given as `exception` parameter.
+        """Method called every time when event loop ends with EventLoopExit or
+        any other exception. If any exception other than ExitEventLoop has been
+         raised, it is given as `exception` parameter.
 
-        - If True is returned, exception is suppressed. Otherwise, it will be
-          raised further.
-        - If no exception is given, return value is simply ignored and nothing
-          should be raised further.
+        Args:
+            self
+            exception: BaseException | None: Exception raised by event handling
+             function or None if `ExitEventLoop` was raised.
 
-        :param exception: Exception raised by event handling function or None
-                          if ExitEventLoop was raised.
-        :return: Suppress exception flag
+        Returns:
+            A boolean value
         """
         return False
 
@@ -100,17 +176,12 @@ class EventHandler(t.Generic[_EventT]):
 
             await self.after_event_loop()
 
-    async def on_loop_close_request(
-        self, event: "LoopCloseRequestEvent", /
-    ) -> None:
+    @handling_method(LoopCloseRequestEvent)
+    async def on_loop_close_request(self, _: LoopCloseRequestEvent, /) -> None:
         raise exc.EventLoopExit()
 
 
-class EventHandlersRunner(
-    t.Generic[_EventT],
-    EventHandler[_EventT | HandlersRunnerFlowEvent],
-    abc.ABC,
-):
+class EventHandlersRunner(EventHandler, abc.ABC):
     def __init__(self):
         super().__init__()
         self._runner_lock = asyncio.Lock()
@@ -123,9 +194,7 @@ class EventHandlersRunner(
     def running(self) -> bool:
         return self._runner_lock.locked()
 
-    async def run_event_loop(
-        self,
-    ) -> None:
+    async def run_event_loop(self) -> None:
         async with self._runner_lock:
             main_loop = asyncio.create_task(super().run_event_loop())
 
@@ -158,7 +227,7 @@ class EventHandlersRunner(
             exceptions = []
 
             if first_exited is not main_loop:
-                SubLoopsCloseRequestEvent().emit_to(self)
+                self.receive(SubLoopsCloseRequestEvent())
 
             # If main loop is exited, then it emitted close request to sub handlers
             if sub_handlers_wait is not None:
@@ -169,7 +238,7 @@ class EventHandlersRunner(
                     except Exception as e:
                         exceptions.append(e)
 
-            LoopCloseRequestEvent().emit_to(self)
+            self.receive(LoopCloseRequestEvent())
 
             try:
                 await main_loop
@@ -187,7 +256,7 @@ class EventHandlersRunner(
 
     async def emit_sub_loop_close_requests(self) -> None:
         for handler in await self.get_sub_handlers():
-            LoopCloseRequestEvent().emit_to(handler)
+            handler.receive(LoopCloseRequestEvent())
 
     async def after_event_loop(
         self, exception: BaseException | None = None
@@ -195,7 +264,8 @@ class EventHandlersRunner(
         await self.emit_sub_loop_close_requests()
         return await super().after_event_loop(exception)
 
+    @handling_method(SubLoopsCloseRequestEvent)
     async def on_sub_loops_close_request(
-        self, event: SubLoopsCloseRequestEvent, /
+        self, _: SubLoopsCloseRequestEvent, /
     ) -> None:
         await self.emit_sub_loop_close_requests()
--- pyeventflow/proto.py
+++ pyeventflow/proto.py
@@ -1,17 +1,14 @@
 import typing as t
 
+from pyeventflow import exc
+
 if t.TYPE_CHECKING:
-    from pyeventflow.event import Event
     from pyeventflow.handler import EventHandler
 
 
-_EventHandlerT_co = t.TypeVar(
-    "_EventHandlerT_co", bound="EventHandler", covariant=True
-)
-
-
-class EventHandlerMethod(t.Protocol):
-    def __call__(  # type: ignore
-        self: _EventHandlerT_co, event: "Event", /
-    ) -> t.Awaitable[None]:
+# TODO: Provide generic type for supported event types
+class EventHandlingFunction(t.Protocol):
+    def __call__(
+        self, handler: "EventHandler", event: t.Any, /
+    ) -> t.Awaitable[None | exc.EventLoopExit | Exception]:
         ...
